6c31ba386013a93571adbc324253a3e99e314e83
gunjanak <janak.klal@gmail.com>
2025-04-03 18:19:28 +0545

    templates added

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
- 
+
 @app.route('/')
 def home():
-    return "Hello"
- 
+    return render_template('index.html', dynamic_text="This is the end or the beginning!")
+
 if __name__ == '__main__':
     app.run()
